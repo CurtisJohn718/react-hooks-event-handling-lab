7a844db19912ae5e6854d908ce3a6fffce6402c5
Curtis <curtisjohn718@aol.com>
2025-02-28 15:34:48 -0500

    React Hooks Events Handling Lab

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Keypad from "./Keypad";
+import EyesOnMe from "./EyesOnMe";
+
+function App() {
+    return (
+        <div>
+           <h1>Event Listeners in React</h1>
+           <Keypad />
+           <EyesOnMe /> 
+        </div>
+    );
+}
+
+export default App;
